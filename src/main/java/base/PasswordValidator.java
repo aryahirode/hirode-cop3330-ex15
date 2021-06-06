c390d444b490401287ec1177bcef80df5c80843d
Arya Hirode <aryahirode2001@gmail.com>
2021-06-06 14:09:17 -0400

    Complete

---
+++ src/main/java/base/PasswordValidator.java
@@ -0,0 +1,33 @@
+package base;
+
+import java.util.Scanner;
+
+public class PasswordValidator {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        PasswordValidator myApp = new PasswordValidator();
+        myApp.output();
+    }
+
+    private String usernamePrompt() {
+        System.out.print("What is your username? ");
+        return in.nextLine();
+    }
+
+    private String passwordPrompt() {
+        System.out.print("What is the password? ");
+        return in.nextLine();
+    }
+
+    private void output() {
+        String password = "abc$123";
+        String inputUsername = usernamePrompt();
+        String inputPassword = passwordPrompt();
+        if(inputPassword.equals(password)) {
+            System.out.println("Welcome!");
+        }
+        else {
+            System.out.println("I don't know you.");
+        }
+    }
+}
